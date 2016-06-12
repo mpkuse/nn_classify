6fba388cdd563a863af2bc7493d797d19cfc1c6b
Manohar Kuse <mpkuse@ust.hk>
2016-06-12 14:07:59 +0800

    The basic gradient descent works.
    TODO:
    1. eval_performance
    2. separate train-set and test-set
    3. Stochastic gradient in place of full gradient

--- nn_main.m
+++ nn_main.m
@@ -1,5 +1,6 @@
 %% Learn-Test 1-layer hidden neural network for 3 category classification
-
+clear all;
+close all;
 %% Generate Data
 nClass = 3;
 dataPerClass = 100;
@@ -14,8 +15,38 @@ b1 = randn( 1, h );
 W2 = randn( h, nClass );
 b2 = randn( 1, nClass );
 
-X = D( 10, : );
-y = L_nn( 10, :);
-[ u1, u2, u3, u4, u5, L ] = forward_pass( X, W1, b1, W2, b2, y );
 
-backward_pass( X, W1, b1, W2, b2, y,   u1, u2, u3, u4, u5, L );
+%% Iterations
+step = 0.001
+for itr = 1:600
+    
+    a_dL_dW1 = zeros( dim, h );
+    a_dL_db1 = zeros( 1, h );
+    a_dL_dW2 = zeros( h, nClass );
+    a_dL_db2 = zeros( 1, nClass );
+    a_cost = 0;
+    
+    % going over data to compute gradient and total cost function
+    % TODO for speed should actually go over random indices only (SGD)
+    for e=1:size(D,1) 
+        X = D( e, : );
+        y = L_nn( e, :);
+        
+        [ u1, u2, u3, u4, u5, L ] = forward_pass( X, W1, b1, W2, b2, y );        
+        [ dL_dW1, dL_db1, dL_dW2, dL_db2 ] = backward_pass( X, W1, b1, W2, b2, y,   u1, u2, u3, u4, u5, L );
+        
+        a_dL_dW1 = a_dL_dW1 + dL_dW1;
+        a_dL_db1 = a_dL_db1 + dL_db1;
+        a_dL_dW2 = a_dL_dW2 + dL_dW2;
+        a_dL_db2 = a_dL_db2 + dL_db2;
+        a_cost = a_cost + L;
+    end
+    
+    % gradient descent (update params)
+    W1 = W1 - step * a_dL_dW1;
+    b1 = b1 - step * a_dL_db1;
+    W2 = W2 - step * a_dL_dW2;
+    b2 = b2 - step * a_dL_db2;
+    
+    display( sprintf( '%d : %f', itr, a_cost ) );
+end
